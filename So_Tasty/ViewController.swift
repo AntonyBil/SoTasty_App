9d3b7ad2c9ca6394d110997b9f1106a6d043071b
AntonyBil <beletskiy.ant@gmail.com>
2023-05-01 22:13:50 +0300

    Initial commit

---
+++ So_Tasty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  So_Tasty
+//
+//  Created by apple on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
