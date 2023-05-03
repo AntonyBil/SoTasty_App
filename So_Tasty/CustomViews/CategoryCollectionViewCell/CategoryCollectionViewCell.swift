1bdf39e3ffbebd269fb25ca5c8f46274150968ea
AntonyBil <beletskiy.ant@gmail.com>
2023-05-03 07:19:38 +0300

    Create custom CollectionViewCell

---
+++ So_Tasty/CustomViews/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCollectionViewCell.swift
+//  So_Tasty
+//
+//  Created by apple on 03.05.2023.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLbl: UILabel!
+    
+    func setup(category: DishCategory) {
+        
+    }
+}
---
+++ So_Tasty/Models/Dishcategory.swift
@@ -0,0 +1,14 @@
+//
+//  Dishcategory.swift
+//  So_Tasty
+//
+//  Created by apple on 03.05.2023.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id: String
+    let name: String
+    let image: String
+}
