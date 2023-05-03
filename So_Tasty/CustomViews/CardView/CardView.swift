117e907b6820130dd29acf7f193d3be66fa5343c
AntonyBil <beletskiy.ant@gmail.com>
2023-05-03 19:22:49 +0300

    Work with shadows and cell spasing in the categoryCollectionView

---
+++ So_Tasty/CustomViews/CardView/CardView.swift
@@ -0,0 +1,29 @@
+//
+//  CardView.swift
+//  So_Tasty
+//
+//  Created by apple on 03.05.2023.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialSetup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialSetup()
+    }
+    
+    private func initialSetup() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.cornerRadius = 10
+        layer.shadowOpacity = 0.1
+        cornerRadius = 10
+    }
+}
