6e09db7dffc2ef87fdd786060485dbc428c88752
AntonyBil <beletskiy.ant@gmail.com>
2023-05-07 18:43:07 +0300

    Make list Dish View Controller

--- So_Tasty/Views/ListDishes/ListDishesViewController.swift
+++ So_Tasty/Views/ListDishes/ListDishesViewController.swift
@@ -11,6 +11,8 @@ class ListDishesViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var category: DishCategory!
+    
     var dishes: [Dish] = [
         .init(id: "id1", name: "Garri", description: "This is the best i have ever tasted", image: "https://picsum.photos/100/200", calories: 55),
         .init(id: "id1", name: "Indomie", description: "This is the best i have ever tasted, This is the best i have ever tasted, This is the best i have ever tasted, This is the best i have ever tasted, This is the best i have ever tasted, This is the best i have ever tasted", image: "https://picsum.photos/100/200", calories: 220),
@@ -20,22 +22,34 @@ class ListDishesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = category.name
+        registerCells()
 
     }
     
-    private func registerCell() {
+    private func registerCells() {
         tableView.register(UINib(nibName: DishListTableViewCell.identifire, bundle: nil), forCellReuseIdentifier: DishListTableViewCell.identifire)
     }
     
 }
 
-extension ListDishesViewController: UITableViewDelegate, UITableViewDataSource {
+extension ListDishesViewController: UITableViewDelegate,
+                                    UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return dishes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: DishListTableViewCell.identifire, for: indexPath) as! DishListTableViewCell
+        cell.setup(dish: dishes[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = DishDetailViewController.instantiate()
+        controller.dish = dishes[indexPath.row]
+        navigationController?.pushViewController(controller, animated: true)
     }
     
     
